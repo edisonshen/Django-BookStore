c183e2f34e28b59ab4ecd0584d41520425b7d763
Feng Shen <edisonshen87@gmail.com>
2016-11-05 23:29:29 -0400

    add social auth

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social.apps.django_app.default',
     'registration',
     'store',
 )
@@ -65,6 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social.apps.django_app.context_processors.backends',
+                'social.apps.django_app.context_processors.login_redirect',
             ],
         },
     },
@@ -72,7 +75,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bookstore.wsgi.application'
 
-
+AUTHENTICATION_BACKENDS = {
+    'social.backends.facebook.FacebookOAuth2',
+    'django.contrib.auth.backends.ModelBackend'
+}
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
@@ -116,3 +122,7 @@ EMAIL_HOST_PASSWORD = "gx2268672sf"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL="edisonshen@fengshen.com"
+
+# Auth - Facebook
+SOCIAL_AUTH_FACEBOOK_KEY = '361223534215426'
+SOCIAL_AUTH_FACEBOOK_SECRET = '91f9b88585ce651cc7ef67ae4a7fe437'
